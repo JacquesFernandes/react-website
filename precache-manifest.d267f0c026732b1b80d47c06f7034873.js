b47cfd4fa237c803fb434190dcd4d2a82a25e3e0
Jacques 'Lawrenz' Fernandes <jacques@castiko.com>
2018-11-19 23:50:58 +0000

    Updates

---
+++ precache-manifest.d267f0c026732b1b80d47c06f7034873.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-website/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4bd2eb48bc79ce71ea7a",
+    "url": "/react-website/static/js/runtime~main.4bd2eb48.js"
+  },
+  {
+    "revision": "fff0a87cedf52fa8139e",
+    "url": "/react-website/static/js/main.fff0a87c.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/react-website/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "fff0a87cedf52fa8139e",
+    "url": "/react-website/static/css/main.ee381b48.chunk.css"
+  },
+  {
+    "revision": "90c484a269a6bed28bd1f8b4577d37f3",
+    "url": "/react-website/index.html"
+  }
+];
